3cc567383d824d2f658f6f87bc7c5244b46c4a79
pradeepalapati24 <pradeep.alapati24@sasi.ac.in>
2026-03-10 13:50:33 +0000

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/corelens-ai/",
+  base: "/core-lens-ai/",
   server: {
     host: "::",
     port: 8080,
